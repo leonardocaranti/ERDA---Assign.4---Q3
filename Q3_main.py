7087cb1dd632ac7906d61e0a85b23b16f806df42
osoare <62461768+osoare@users.noreply.github.com>
2020-03-21 11:01:21 +0100

    Update Q3_main.py

--- Q3_main.py
+++ Q3_main.py
@@ -6,6 +6,7 @@ A = np.array([[1,2,3], [4,5,6], [7,8,9], [2,3,6]])
 f = np.array([[1],[2],[3],[4],[5],[6],[7],[8],[9],[10]])
 
 A_inv = np.linalg.inv(A)
+
 a = np.dot(A_inv, np.dot(f))
 
 print(a)
@@ -16,6 +17,3 @@ def squares(a_s):
         sum += (np.dot(a_s[i], A[i]) - f[i])
     return sum
 
-print()
-x = sp.optimize.least_squares(squares, [3.6, 4.11, -7.9]).x()
-print(x)
