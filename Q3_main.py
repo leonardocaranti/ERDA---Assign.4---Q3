6718bfbcdffcb0e169b5e751d0cf9e59dc487998
osoare <62461768+osoare@users.noreply.github.com>
2020-03-21 10:56:13 +0100

    Update Q3_main.py

--- Q3_main.py
+++ Q3_main.py
@@ -8,7 +8,7 @@ A = np.array([[1,2,3], [4,5,6], [7,8,9], [2,3,6]])
 f = np.array([func(A[0,0], A[0,1], A[0,2]), func(A[1,0], A[1,1], A[1,2]), func(A[2,0], A[2,1], A[2,2]), func(A[3,0], A[3,1], A[3,2])])
 A_t = A.transpose()
 
-A_t_a_inv = np.linalg.inv(A_t_A)
+A_inv = np.linalg.inv(A)
 a = np.dot(A_t_a_inv, np.dot(A_t, f))
 
 print(a)
